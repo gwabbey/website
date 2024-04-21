33d3ad91cdd6075931364ddeb8525284889e9f91
gab <mail@g3b.dev>
2024-04-21 19:16:06 +0200

    use flex layout in projects page
    refactor(projects): use flexbox layout for the projects section
    feat(projects): add a second project card

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -6,42 +6,75 @@ export default function Projects() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <Flex gap="md" justify="center" align="center" direction="column">
         <Title title={"projects"} />
-        <div style={{ width: "100%", maxWidth: 800, textAlign: "center" }}>
-          <Text size={"xl"}>some of my projects</Text>
-          <Card shadow="sm" padding="lg" radius="md" m={16} withBorder>
-            <Card.Section>
-              <Image
-                src="/images/move-automation.png"
-                height={80}
-                width={80}
-                alt="Gestione Move"
-              />
-            </Card.Section>
+        <Text size={"xl"}>some of my projects</Text>
+        <Flex
+          gap="md"
+          justify="center"
+          align="center"
+          wrap={"wrap"}
+          direction={{ base: "column", sm: "row" }}
+        >
+          <div style={{ maxWidth: 500, textAlign: "center" }}>
+            <Card shadow="sm" padding="lg" radius="md" m={16} withBorder>
+              <Card.Section>
+                <Image src="/images/move-automation.webp" alt="Gestione Move" />
+              </Card.Section>
 
-            <Group justify="center" mt="md" mb="xs">
-              <Text fw={500} fs={"xl"}>
-                Gestione Move
+              <Group justify="center" mt="md" mb="xs">
+                <Text fw={500} fs={"xl"}>
+                  Gestione Move
+                </Text>
+              </Group>
+
+              <Text size="sm" c="dimmed">
+                A web application for managing a small business
               </Text>
-            </Group>
 
-            <Text size="sm" c="dimmed">
-              A web application for managing a small business
-            </Text>
+              <Card.Section p={16}>
+                <Button
+                  component={"a"}
+                  href="gestione.moveautomation.it"
+                  target="_blank"
+                  size="md"
+                  radius="xl"
+                  w={"50%"}
+                >
+                  Visit
+                </Button>
+              </Card.Section>
+            </Card>
+          </div>
+          <div style={{ maxWidth: 500, textAlign: "center" }}>
+            <Card shadow="sm" padding="lg" radius="md" m={16} withBorder>
+              <Card.Section>
+                <Image src="/images/move-automation.webp" alt="Gestione Move" />
+              </Card.Section>
 
-            <Card.Section p={16}>
-              <Button
-                component={"a"}
-                href="gestione.moveautomation.it"
-                target="_blank"
-                size="md"
-                radius="xl"
-                w={"50%"}
-              >
-                Visit
-              </Button>
-            </Card.Section>
-          </Card>
-        </div>
+              <Group justify="center" mt="md" mb="xs">
+                <Text fw={500} fs={"xl"}>
+                  Gestione Move
+                </Text>
+              </Group>
+
+              <Text size="sm" c="dimmed">
+                A web application for managing a small business
+              </Text>
+
+              <Card.Section p={16}>
+                <Button
+                  component={"a"}
+                  href="gestione.moveautomation.it"
+                  target="_blank"
+                  size="md"
+                  radius="xl"
+                  w={"50%"}
+                >
+                  Visit
+                </Button>
+              </Card.Section>
+            </Card>
+          </div>
+        </Flex>
       </Flex>
     </div>
   );
